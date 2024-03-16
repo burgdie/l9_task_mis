88f60cb82ce59c1e222732692c6fa98bc4f1ceb0
Dieter Burgstaller <72778774+burgdie@users.noreply.github.com>
2024-03-16 10:06:21 +0100

    #8 Route Basics -- Section 2 Laravel Basics -- Master Laravel 9 with Vuejs

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,42 +18,26 @@
 		  		<a href="#" class="img logo rounded-circle mb-5" style="background-image: url(asset('sidebar/images/logo.jpg'));"></a>
 	        <ul class="list-unstyled components mb-5">
 	          <li class="active">
-	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Management</a>
 	            <ul class="collapse list-unstyled" id="homeSubmenu">
                 <li>
-                    <a href="#">Home 1</a>
+                    <a href="{{ route('departmentsIndex') }}">Departments</a>
                 </li>
                 <li>
-                    <a href="#">Home 2</a>
+                    <a href="{{ route('usersIndex') }}">Users</a>
                 </li>
                 <li>
-                    <a href="#">Home 3</a>
-                </li>
-	            </ul>
-	          </li>
-	          <li>
-	              <a href="#">About</a>
-	          </li>
-	          <li>
-              <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-              <ul class="collapse list-unstyled" id="pageSubmenu">
-                <li>
-                    <a href="#">Page 1</a>
+                    <a href="#">Roles</a>
                 </li>
                 <li>
-                    <a href="#">Page 2</a>
-                </li>
-                <li>
-                    <a href="#">Page 3</a>
+                    <a href="#">Permissions</a>
                 </li>
               </ul>
 	          </li>
 	          <li>
-              <a href="#">Portfolio</a>
+	              <a href="#">Task Inbox</a>
 	          </li>
-	          <li>
-              <a href="#">Contact</a>
-	          </li>
+	         
 	        </ul>
 
 	        <div class="footer">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+
+Route::get('departments/index', function() {
+    return view('management.departments.index');
+})->name('departmentsIndex');
+
+Route::get('users/index', function() {
+    return view('management.users.index');
+})->name('usersIndex');
