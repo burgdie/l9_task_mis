f0019f35b1da06bc18879ea7a884d2f20e9e41a7
Dieter Burgstaller <72778774+burgdie@users.noreply.github.com>
2024-03-18 22:21:10 +0100

    #26 Vue JS Data Properties -- Section 3 Vue JS Basics -- Master Laravel 9 with Vuejs

--- resources/js/components/Departments.vue
+++ resources/js/components/Departments.vue
@@ -1,6 +1,90 @@
 <template>
     <h1>Departments List</h1>
+
+    <!-- Button trigger modal -->
+    <button
+        type="button"
+        class="btn btn-success"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+    >
+        Create New Department
+    </button>
+
+    <!-- Modal -->
+    <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+    >
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">
+                        Modal title
+                    </h1>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    name="name"
+                                    v-model="name"
+                                />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="name">Director</label>
+                                <select
+                                    name="director_id"
+                                    class="form-control"
+                                    v-model="director_id"
+                                    autocomplete="on"
+                                >
+                                    <option value="">Select a person</option>
+                                    <option value="1">IT Director</option>
+                                    <option value="2">HR Director</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button
+                        type="button"
+                        class="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                    >
+                        Close
+                    </button>
+                    <button type="button" class="btn btn-success">
+                        Save changes
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            name: "",
+            director_id: "",
+        };
+    },
+};
 </script>
